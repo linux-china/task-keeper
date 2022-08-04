c9e380bcbaa454afb16ba61328c9b953d3414925
linux_china <libing.chen@gmail.com>
2022-08-04 16:19:48 -0700

    chore: use error-stack to error

--- src/keeper.rs
+++ src/keeper.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 use std::process::{Command, Output, Stdio};
+use error_stack::{IntoReport, Result, ResultExt};
+use crate::errors::KeeperError;
 
 pub const RUNNERS: &'static [&'static str] = &["make", "npm", "deno", "just"];
 
-pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose: bool) -> anyhow::Result<Output> {
+pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
     let mut command = Command::new(runner);
     if extra_args.len() > 0 {
         command.args(extra_args);
@@ -16,11 +18,13 @@ pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
-        .output()?;
+        .output()
+        .report()
+        .change_context(KeeperError::FailedToRunTasks(format!("{:?}", command)))?;
     Ok(output)
 }
 
-pub fn list_tasks() -> anyhow::Result<HashMap<String, Vec<String>>> {
+pub fn list_tasks() -> Result<HashMap<String, Vec<String>>, KeeperError> {
     let mut tasks = HashMap::new();
     tasks.insert("just".to_string(), vec!["hello".to_string(), "hello2".to_string()]);
     tasks.insert("make".to_string(), vec!["clean".to_string(), "compile".to_string()]);
