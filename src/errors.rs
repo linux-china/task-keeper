7a5add7fabbd831ffa4284dde28fb2e4a2c76a43
linux_china <libing.chen@gmail.com>
2022-08-04 16:19:21 -0700

    feat: errors module

---
+++ src/errors.rs
@@ -0,0 +1,22 @@
+use thiserror::Error as ThisError;
+
+/// errors for config component: app-100
+#[derive(ThisError, Debug)]
+pub enum KeeperError {
+    #[error("TK-100404: task not found: {0}")]
+    TaskNotFound(String),
+    #[error("TK-100500: failed to run tasks: {0}")]
+    FailedToRunTasks(String),
+    #[error("TK-201001: failed to parse Makefile: {0}")]
+    InvalidMakefile(String),
+    #[error("TK-202001: failed to parse package.json: {0}")]
+    InvalidPackageJson(String),
+    #[error("TK-203001: failed to parse package.json: {0}")]
+    InvalidDenoJson(String),
+    #[error("TK-204001: failed to parse package.json: {0}")]
+    InvalidFleetRunJson(String),
+    #[error("TK-205001: failed to parse Justfile: {0}")]
+    InvalidCodeLaunchJson(String),
+    #[error("TK-206001: failed to parse Justfile: {0}")]
+    InvalidJustfile(String),
+}
