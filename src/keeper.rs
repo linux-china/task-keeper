898180320e1455fa4acd7e6c2bac8f6e611d52fd
linux_china <libing.chen@gmail.com>
2022-08-04 16:44:12 -0700

    refactor: use Task from models.rs

--- src/keeper.rs
+++ src/keeper.rs
@@ -2,25 +2,10 @@ use std::collections::HashMap;
 use std::process::{Command, Output, Stdio};
 use error_stack::{IntoReport, Result, ResultExt};
 use crate::errors::KeeperError;
+use crate::models::Task;
+use crate::task;
 
 pub const RUNNERS: &'static [&'static str] = &["make", "npm", "deno", "just"];
-
-macro_rules! task {
-    ($name:expr, $runner:expr) => {
-       Task { name: $name.to_owned(), runner: $runner.to_owned(), description: "".to_owned()}
-    };
-    ($name:expr, $runner:expr, $description:expr) => {
-       Task { name: $name.to_owned(), runner: $runner.to_owned(), description: $description.to_owned()}
-    };
-}
-
-
-#[derive(Debug)]
-pub struct Task {
-    pub name: String,
-    pub runner: String,
-    pub description: String,
-}
 
 pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
     let mut command = Command::new(runner);
@@ -43,7 +28,7 @@ pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose
 
 pub fn list_tasks() -> Result<HashMap<String, Vec<Task>>, KeeperError> {
     let mut tasks = HashMap::new();
-    tasks.insert("just".to_string(), vec![task!("hello","just","Say Hello"),task!("hello2","just")]);
-    tasks.insert("make".to_string(), vec![task!("hello","make"),task!("hello2","make")]);
+    tasks.insert("just".to_string(), vec![task!("hello","just","Say Hello"), task!("hello2","just")]);
+    tasks.insert("make".to_string(), vec![task!("hello","make"), task!("hello2","make")]);
     Ok(tasks)
 }
