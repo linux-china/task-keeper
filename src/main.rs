bebaccc396ca1306943e8b386e13673e5351a3d9
linux_china <libing.chen@gmail.com>
2022-08-04 15:09:53 -0700

    feat: main.rs with clap cli

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use crate::app::build_app;
+use crate::keeper::run_tasks;
+
+mod app;
+mod keeper;
+
+fn main() {
+    let app = build_app();
+    let matches = app.get_matches();
+    let verbose = matches.is_present("verbose");
+
+    // list tasks
+    if matches.is_present("list") {
+        println!("{}", "list tasks");
+        return;
+    }
+    // migrate tasks
+    if matches.is_present("from") && matches.is_present("to") {
+        println!("{}", "migrate tasks");
+        return;
+    }
+    // run tasks
+    if matches.is_present("tasks") {
+        let mut tasks = matches.values_of("tasks").unwrap().collect::<Vec<&str>>();
+        let mut extra_args = vec![];
+        let double_dash = tasks.iter().position(|x| *x == "--");
+        if let Some(index) = double_dash {
+            extra_args = tasks.split_off(index)[1..].to_vec();
+        }
+        let runner = matches.value_of("runner").unwrap_or("just");
+        run_tasks(runner, &tasks, &extra_args, verbose).unwrap();
+        return;
+    }
+
+    // display help message
+    build_app().print_help().unwrap();
+}
