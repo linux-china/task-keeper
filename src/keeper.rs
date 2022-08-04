8f913b8b94867851010286cae29004999cdb8e40
linux_china <libing.chen@gmail.com>
2022-08-04 15:37:31 -0700

    feat: add list_tasks

--- src/keeper.rs
+++ src/keeper.rs
@@ -1,5 +1,7 @@
-use std::path::Path;
+use std::collections::HashMap;
 use std::process::{Command, Output, Stdio};
+
+pub const RUNNERS: &'static [&'static str] = &["make", "npm", "deno", "just"];
 
 pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose: bool) -> anyhow::Result<Output> {
     let mut command = Command::new(runner);
@@ -16,4 +18,11 @@ pub fn run_tasks(runner: &str, task_names: &[&str], extra_args: &[&str], verbose
         .stderr(Stdio::inherit())
         .output()?;
     Ok(output)
+}
+
+pub fn list_tasks() -> anyhow::Result<HashMap<String, Vec<String>>> {
+    let mut tasks = HashMap::new();
+    tasks.insert("just".to_string(), vec!["hello".to_string(), "hello2".to_string()]);
+    tasks.insert("make".to_string(), vec!["clean".to_string(), "compile".to_string()]);
+    Ok(tasks)
 }
