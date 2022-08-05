92d5ef3e5747eff90d97f540a642031f93285120
linux_china <libing.chen@gmail.com>
2022-08-04 19:21:26 -0700

    chore: adjust error msg

--- src/errors.rs
+++ src/errors.rs
@@ -9,14 +9,14 @@ pub enum KeeperError {
     FailedToRunTasks(String),
     #[error("TK-201001: failed to parse Makefile: {0}")]
     InvalidMakefile(String),
-    #[error("TK-202001: failed to parse package.json: {0}")]
-    InvalidPackageJson(String),
+    #[error("TK-202001: failed to parse package.json")]
+    InvalidPackageJson,
     #[error("TK-203001: failed to parse package.json: {0}")]
     InvalidDenoJson(String),
     #[error("TK-204001: failed to parse package.json: {0}")]
     InvalidFleetRunJson(String),
     #[error("TK-205001: failed to parse Justfile: {0}")]
     InvalidCodeLaunchJson(String),
-    #[error("TK-206001: failed to parse Justfile: {0}")]
-    InvalidJustfile(String),
+    #[error("TK-206001: failed to parse Justfile")]
+    InvalidJustfile,
 }
