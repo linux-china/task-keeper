c9d1121018f703a914aa9ef834e310f380af003a
linux_china <libing.chen@gmail.com>
2022-08-05 17:18:11 -0700

    feat: add markdown runner

--- src/runners/mod.rs
+++ src/runners/mod.rs
@@ -8,14 +8,16 @@ pub mod taskspy;
 pub mod taskfileyml;
 pub mod makefiletoml;
 pub mod procfile;
+pub mod markdown;
 
 use std::collections::HashMap;
 use std::process::{Command, Output, Stdio};
-use error_stack::{IntoReport, Result, ResultExt};
+use colored::Colorize;
+use error_stack::{IntoReport, report, Result, ResultExt};
 use crate::errors::KeeperError;
 use which::which;
 
-pub const RUNNERS: &'static [&'static str] = &["rake", "invoke", "task", "cargo-make", "just", "make", "procfile", "npm", "deno", "fleet"];
+pub const RUNNERS: &'static [&'static str] = &["rake", "invoke", "task", "cargo-make", "just", "make", "procfile", "npm", "deno", "fleet", "markdown"];
 
 pub fn is_command_available(command_name: &str) -> bool {
     which(command_name).is_ok()
@@ -23,6 +25,18 @@ pub fn is_command_available(command_name: &str) -> bool {
 
 pub fn run_command(command_name: &str, args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
     run_command_with_env_vars(command_name, args, &None, verbose)
+}
+
+pub fn run_command_line(command_line: &str, verbose: bool) -> Result<Output, KeeperError> {
+    let command_and_args = shlex::split(command_line).unwrap();
+    let command_name = &command_and_args[0];
+    let args: Vec<&str> = command_and_args[1..].iter().map(AsRef::as_ref).collect();
+    if is_command_available(&command_name) {
+        run_command(&command_name, &args, verbose)
+    } else {
+        println!("{}", format!("{} is not available to run '{}'", command_name, command_line).bold().red());
+        Err(report!(KeeperError::CommandNotFound(command_name.to_string())))
+    }
 }
 
 pub fn run_command_with_env_vars(command_name: &str, args: &[&str], env_vars: &Option<HashMap<String, String>>, verbose: bool) -> Result<Output, KeeperError> {
