217d37a823dde22eec41b7e66cd2ed3a276b602d
linux_china <libing.chen@gmail.com>
2022-08-07 13:50:55 -0700

    feat: add taskshell

--- src/runners/mod.rs
+++ src/runners/mod.rs
@@ -9,6 +9,7 @@ pub mod taskfileyml;
 pub mod makefiletoml;
 pub mod procfile;
 pub mod markdown;
+pub mod taskshell;
 
 use std::collections::HashMap;
 use std::process::{Command, Output, Stdio};
@@ -17,7 +18,25 @@ use error_stack::{IntoReport, report, Result, ResultExt};
 use crate::errors::KeeperError;
 use which::which;
 
-pub const RUNNERS: &'static [&'static str] = &["rake", "invoke", "task", "cargo-make", "just", "make", "procfile", "npm", "deno", "fleet", "markdown"];
+pub const RUNNERS: &'static [&'static str] = &["rake", "invoke", "task", "cargo-make", "just", "make", "proc", "npm", "deno", "shell", "fleet", "markdown"];
+
+pub fn get_runner_file_name(runner: &str) -> &'static str {
+    match runner {
+        "rake" => "Rakefile",
+        "invoke" => "tasks.py",
+        "task" => "Taskfile.yml",
+        "cargo-make" => "Makefile.toml",
+        "just" => "Justfile",
+        "make" => "Makefile",
+        "proc" => "Procfile",
+        "npm" => "package.json",
+        "deno" => "deno.json",
+        "fleet" => ".fleet/run.json",
+        "shell" => "task.sh",
+        "markdown" => "README.md",
+        _ => "unknown",
+    }
+}
 
 pub fn is_command_available(command_name: &str) -> bool {
     which(command_name).is_ok()
