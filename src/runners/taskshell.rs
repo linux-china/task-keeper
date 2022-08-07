0230b8ab3b77d2bc9161390697653795474e9ace
linux_china <libing.chen@gmail.com>
2022-08-07 13:50:31 -0700

    feat: shell runer for task.sh file

---
+++ src/runners/taskshell.rs
@@ -0,0 +1,68 @@
+use std::io::{BufRead, BufReader};
+use std::process::Output;
+use crate::errors::KeeperError;
+use crate::models::Task;
+use crate::runners::{run_command, capture_command_output};
+use crate::task;
+use error_stack::{Result};
+use regex::Regex;
+use which::which;
+
+pub fn is_available() -> bool {
+    std::env::current_dir()
+        .map(|dir| dir.join("task.sh").exists())
+        .unwrap_or(false)
+}
+
+pub fn is_command_available() -> bool {
+    true
+}
+
+pub fn list_tasks() -> Result<Vec<Task>, KeeperError> {
+    let makefile_meta_text = capture_command_output("./task.sh", &[])
+        .map(|output| {
+            String::from_utf8(output.stdout).unwrap_or("{}".to_owned())
+        })?;
+    let tasks: Vec<Task> = BufReader::new(makefile_meta_text.as_bytes())
+        .lines()
+        .filter(|line| {
+            line.is_ok() && line.as_ref().unwrap().starts_with("commands:")
+        })
+        .map(|line| line.unwrap()[9..].to_owned())
+        .flat_map(|line| {
+            line.split_whitespace().into_iter()
+                .map(|task_name| {
+                    task!(task_name, "shell")
+                })
+                .collect::<Vec<Task>>()
+        })
+        .collect::<Vec<Task>>();
+    Ok(tasks)
+}
+
+pub fn run_task(task: &str, extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
+    let mut args = vec![];
+    args.push(task);
+    args.extend(extra_args);
+    run_command("./task.sh", &args, verbose)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        if let Ok(tasks) = list_tasks() {
+            println!("{:?}", tasks);
+        }
+    }
+
+    #[test]
+    fn test_run() {
+        if let Ok(output) = run_task("start", &[], true) {
+            let status_code = output.status.code().unwrap_or(0);
+            println!("exit code: {}", status_code);
+        }
+    }
+}
