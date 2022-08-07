591dffdfbf77f9be093734af5becf956690e1b3e
linux_china <libing.chen@gmail.com>
2022-08-07 15:25:47 -0700

    chore: polishing

--- src/managers/maven.rs
+++ src/managers/maven.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub fn get_task_command_map() -> HashMap<String, String> {
     let mut task_command_map = HashMap::new();
-    task_command_map.insert("init".to_string(), "archetype:generate".to_string());
+    task_command_map.insert("init".to_string(), "mvn archetype:generate".to_string());
     task_command_map.insert("compile".to_string(), "mvn compile testCompile".to_string());
     task_command_map.insert("build".to_string(), "mvn -DskipTests package".to_string());
     task_command_map.insert("test".to_string(), "mvn test".to_string());
