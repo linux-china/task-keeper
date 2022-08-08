68486307abcd6cbc323c95ee26a8c18aec5bf153
linux_china <libing.chen@gmail.com>
2022-08-07 18:28:02 -0700

    feat: implement task run for manager

--- src/managers/mod.rs
+++ src/managers/mod.rs
@@ -1,3 +1,8 @@
+use std::process::Output;
+use colored::Colorize;
+use crate::errors::KeeperError;
+use error_stack::{IntoReport, report, Result, ResultExt};
+
 pub mod maven;
 pub mod gradle;
 pub mod npm;
@@ -21,4 +26,65 @@ pub fn get_manager_file_name(runner: &str) -> &'static str {
         "swift" => "Package.swift",
         _ => "unknown",
     }
+}
+
+pub fn run_task(runner: &str, task_name: &str, extra_args: &[&str], verbose: bool) -> Result<(), KeeperError> {
+    println!("{}", format!("[tk] execute {} from {}", task_name, runner).bold().blue());
+    let mut queue: Vec<fn(&str, &[&str], bool) -> Result<Output, KeeperError>> = vec![];
+    if maven::is_available() {
+        if maven::is_command_available() {
+            queue.push(maven::run_task);
+        } else {
+            println!("{}", format!("[tk] maven(https://maven.apache.org/) command not available for pom.xml").bold().red());
+        }
+    }
+    if gradle::is_available() {
+        if gradle::is_command_available() {
+            queue.push(maven::run_task);
+        } else {
+            println!("{}", format!("[tk] gradle(https://gradle.org/) command not available").bold().red());
+        }
+    }
+    if sbt::is_available() {
+        if sbt::is_command_available() {
+            queue.push(sbt::run_task);
+        } else {
+            println!("{}", format!("[tk] sbt(https://www.scala-sbt.org/) command not available for build.sbt").bold().red());
+        }
+    }
+    if npm::is_available() {
+        if npm::is_command_available() {
+            queue.push(npm::run_task);
+        } else {
+            println!("{}", format!("[tk] npm(https://nodejs.org/) command not available for package.json").bold().red());
+        }
+    }
+    if cargo::is_available() {
+        if cargo::is_command_available() {
+            queue.push(cargo::run_task);
+        } else {
+            println!("{}", format!("[tk] cargo(https://gradle.org/) command not available for Cargo.toml").bold().red());
+        }
+    }
+    if composer::is_available() {
+        if composer::is_command_available() {
+            queue.push(composer::run_task);
+        } else {
+            println!("{}", format!("[tk] gradle(https://gradle.org/) command not available for composer.json").bold().red());
+        }
+    }
+    match task_name {
+        "init" => {}
+        "start" => { // only execute start task once
+            if let Some(task) = queue.first() {
+                task(task_name, extra_args, verbose).unwrap();
+            }
+        }
+        _ => {
+            queue.iter().for_each(|task| {
+                task(task_name, extra_args, verbose).unwrap();
+            });
+        }
+    }
+    Ok(())
 }
