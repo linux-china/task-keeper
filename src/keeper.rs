1c2f1b7538973feca9bb8c1800aae159207aa023
linux_china <libing.chen@gmail.com>
2022-08-07 18:29:05 -0700

    feat: add task run for package manager

--- src/keeper.rs
+++ src/keeper.rs
@@ -1,69 +1,69 @@
 use std::collections::HashMap;
 use std::process::{Output};
 use colored::Colorize;
-use error_stack::{report, Result};
+use error_stack::{Result};
 use crate::errors::KeeperError;
 use crate::models::Task;
-use crate::runners;
+use crate::{managers, runners};
 use crate::runners::RUNNERS;
 
-pub fn run_tasks(cli_runner: &str, task_names: &[&str], extra_args: &[&str], verbose: bool) -> Result<i32, KeeperError> {
+pub fn run_tasks(cli_runner: &str, target_task_names: &[&str], extra_args: &[&str], verbose: bool) -> Result<i32, KeeperError> {
     let mut task_count = 0;
-    let all_tasks = list_tasks();
+    let all_tasks = list_all_runner_tasks();
     if let Ok(tasks_hashmap) = all_tasks {
         if !cli_runner.is_empty() { //runner is specified
-            if let Some(tasks) = tasks_hashmap.get(cli_runner) {
-                for task_name in task_names {
-                    tasks.iter()
+            if let Some(runner_tasks) = tasks_hashmap.get(cli_runner) {
+                let mut runner_task_found = false;
+                for target_task_name in target_task_names {
+                    runner_tasks.iter()
                         .for_each(|task| {
-                            if task.name.as_str() == *task_name {
+                            if task.name.as_str() == *target_task_name {
                                 task_count += 1;
-                                run_task(cli_runner, task, extra_args, verbose).unwrap();
+                                runner_task_found = true;
+                                run_runner_task(cli_runner, target_task_name, extra_args, verbose).unwrap();
                             }
                         });
+                    // execute package manager task
+                    if !runner_task_found && managers::COMMANDS.contains(target_task_name) {
+                        task_count += 1;
+                        run_manager_task(cli_runner, target_task_name, extra_args, verbose).unwrap();
+                    }
                 }
             }
         } else { //unknown runner
-            for task_name in task_names {
+            for target_task_name in target_task_names {
+                let mut runner_task_found = false;
                 RUNNERS.iter().for_each(|runner| {
                     if let Some(tasks) = tasks_hashmap.get(*runner) {
                         tasks.iter()
                             .for_each(|task| {
-                                if task.name.as_str() == *task_name {
+                                if task.name.as_str() == *target_task_name {
                                     task_count += 1;
-                                    run_task(runner, task, extra_args, verbose).unwrap();
+                                    run_runner_task(runner, target_task_name, extra_args, verbose).unwrap();
                                 }
                             });
                     }
                 });
+                // execute package manager task
+                if !runner_task_found && managers::COMMANDS.contains(target_task_name) {
+                    task_count += 1;
+                    run_manager_task(cli_runner, target_task_name, extra_args, verbose).unwrap();
+                }
             }
         }
     }
     Ok(task_count)
 }
 
-pub fn run_task(runner: &str, task: &Task, extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
-    let task_name = task.name.as_str();
-    println!("{}", format!("[tk] execute {} from {}", task_name, runner).bold().blue());
-    match runner {
-        "npm" => runners::packagejson::run_task(task_name, extra_args, verbose),
-        "just" => runners::justfile::run_task(task_name, extra_args, verbose),
-        "fleet" => runners::fleet::run_task(task_name, extra_args, verbose),
-        "deno" => runners::denojson::run_task(task_name, extra_args, verbose),
-        "make" => runners::makefile::run_task(task_name, extra_args, verbose),
-        "rake" => runners::rakefile::run_task(task_name, extra_args, verbose),
-        "task" => runners::taskfileyml::run_task(task_name, extra_args, verbose),
-        "invoke" => runners::taskspy::run_task(task_name, extra_args, verbose),
-        "cargo-make" => runners::makefiletoml::run_task(task_name, extra_args, verbose),
-        "procfile" => runners::procfile::run_task(task_name, extra_args, verbose),
-        "composer" => runners::composer::run_task(task_name, extra_args, verbose),
-        "markdown" => runners::markdown::run_task(task_name, extra_args, verbose),
-        "shell" => runners::taskshell::run_task(task_name, extra_args, verbose),
-        _ => Err(report!(KeeperError::FailedToRunTasks(format!("unknown runner: {}", runner)))),
-    }
+pub fn run_runner_task(runner: &str, task_name: &str, extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
+    runners::run_task(runner, task_name, extra_args, verbose)
 }
 
-pub fn list_tasks() -> Result<HashMap<String, Vec<Task>>, KeeperError> {
+pub fn run_manager_task(runner: &str, task_name: &str, extra_args: &[&str], verbose: bool) -> Result<(), KeeperError> {
+    managers::run_task(runner, task_name, extra_args, verbose)
+}
+
+pub fn list_all_runner_tasks() -> Result<HashMap<String, Vec<Task>>, KeeperError> {
     let mut tasks = HashMap::new();
     if runners::fleet::is_available() {
         tasks.insert("fleet".to_string(), runners::fleet::list_tasks().unwrap());
@@ -150,8 +150,7 @@ mod tests {
 
     #[test]
     fn test_run_task() {
-        let task = task!("start", "npm");
-        if let Ok(output) = run_task("npm", &task, &[], true) {
+        if let Ok(output) = run_runner_task("npm", "start", &[], true) {
             let status_code = output.status.code().unwrap_or(0);
             println!("exit code: {}", status_code);
         }
