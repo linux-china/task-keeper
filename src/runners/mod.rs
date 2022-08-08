59e1e3116799c515ed1bb38f1008100dc2cd88f0
linux_china <libing.chen@gmail.com>
2022-08-07 17:21:42 -0700

    refactor: add run_task()

--- src/runners/mod.rs
+++ src/runners/mod.rs
@@ -21,6 +21,26 @@ use which::which;
 
 pub const RUNNERS: &'static [&'static str] = &["rake", "invoke", "task", "cargo-make", "just", "make", "proc", "npm", "deno", "composer", "shell", "fleet", "markdown"];
 
+pub fn run_task(runner: &str, task_name: &str, extra_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
+    println!("{}", format!("[tk] execute {} from {}", task_name, runner).bold().blue());
+    match runner {
+        "npm" => packagejson::run_task(task_name, extra_args, verbose),
+        "just" => justfile::run_task(task_name, extra_args, verbose),
+        "fleet" => fleet::run_task(task_name, extra_args, verbose),
+        "deno" => denojson::run_task(task_name, extra_args, verbose),
+        "make" => makefile::run_task(task_name, extra_args, verbose),
+        "rake" => rakefile::run_task(task_name, extra_args, verbose),
+        "task" => taskfileyml::run_task(task_name, extra_args, verbose),
+        "invoke" => taskspy::run_task(task_name, extra_args, verbose),
+        "cargo-make" => makefiletoml::run_task(task_name, extra_args, verbose),
+        "procfile" => procfile::run_task(task_name, extra_args, verbose),
+        "composer" => composer::run_task(task_name, extra_args, verbose),
+        "markdown" => markdown::run_task(task_name, extra_args, verbose),
+        "shell" => taskshell::run_task(task_name, extra_args, verbose),
+        _ => Err(report!(KeeperError::FailedToRunTasks(format!("unknown runner: {}", runner)))),
+    }
+}
+
 pub fn get_runner_file_name(runner: &str) -> &'static str {
     match runner {
         "rake" => "Rakefile",
