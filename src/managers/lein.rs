29fa19d23a04c9526b713985e3511e95d4b41c77
linux_china <libing.chen@gmail.com>
2022-08-10 12:38:35 -0700

    feat: lein package manager module

---
+++ src/managers/lein.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::process::Output;
+use error_stack::{report, Result};
+use which::which;
+use crate::command_utils::{run_command_line};
+use crate::errors::KeeperError;
+
+pub fn is_available() -> bool {
+    std::env::current_dir()
+        .map(|dir| dir.join("project.clj").exists())
+        .unwrap_or(false)
+}
+
+pub fn is_command_available() -> bool {
+    which("lein").is_ok()
+}
+
+pub fn get_task_command_map() -> HashMap<String, String> {
+    let mut task_command_map = HashMap::new();
+    task_command_map.insert("init".to_string(), "lein new app {name}".to_string());
+    task_command_map.insert("install".to_string(), "lein deps".to_string());
+    task_command_map.insert("compile".to_string(), "lein compile".to_string());
+    task_command_map.insert("build".to_string(), "lein uberjar".to_string());
+    task_command_map.insert("start".to_string(), "lein run".to_string());
+    task_command_map.insert("test".to_string(), "lein test".to_string());
+    task_command_map.insert("deps".to_string(), "lein deps :tree".to_string());
+    task_command_map.insert("clean".to_string(), "lein do clean".to_string());
+    task_command_map.insert("outdated".to_string(), "lein outdated".to_string());
+    task_command_map.insert("update".to_string(), "lein outdated --upgrade".to_string());
+    task_command_map
+}
+
+pub fn run_task(task: &str, _task_args: &[&str], _global_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
+    if let Some(command_line) = get_task_command_map().get(task) {
+        run_command_line(command_line, verbose)
+    } else {
+        Err(report!(KeeperError::ManagerTaskNotFound(task.to_owned(), "lein".to_string())))
+    }
+}
