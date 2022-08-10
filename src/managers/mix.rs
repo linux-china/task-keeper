de418e1ad6ae951299076683d2f8b9ed7b5035ec
linux_china <libing.chen@gmail.com>
2022-08-10 14:26:38 -0700

    feat: mix package manager module

---
+++ src/managers/mix.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::process::Output;
+use error_stack::{report, Result};
+use which::which;
+use crate::command_utils::{run_command_line};
+use crate::errors::KeeperError;
+
+pub fn is_available() -> bool {
+    std::env::current_dir()
+        .map(|dir| dir.join("mix.exs").exists())
+        .unwrap_or(false)
+}
+
+pub fn is_command_available() -> bool {
+    which("mix").is_ok()
+}
+
+pub fn get_task_command_map() -> HashMap<String, String> {
+    // https://hexdocs.pm/mix/1.13/Mix.html
+    // https://hexdocs.pm/hex/Mix.Tasks.Hex.html
+    let mut task_command_map = HashMap::new();
+    task_command_map.insert("init".to_string(), "mix new {name}".to_string());
+    task_command_map.insert("install".to_string(), "mix deps.get".to_string());
+    task_command_map.insert("compile".to_string(), "mix compile".to_string());
+    task_command_map.insert("build".to_string(), "hex.build".to_string());
+    task_command_map.insert("deps".to_string(), "mix deps.tree".to_string());
+    task_command_map.insert("doc".to_string(), "mix docs".to_string());
+    task_command_map.insert("clean".to_string(), "mix clean".to_string());
+    task_command_map.insert("start".to_string(), "mix run".to_string());
+    task_command_map.insert("test".to_string(), "mix test".to_string());
+    task_command_map.insert("outdated".to_string(), "mix hex.outdated".to_string());
+    task_command_map.insert("update".to_string(), "mix deps.update --all".to_string());
+    task_command_map
+}
+
+pub fn run_task(task: &str, _task_args: &[&str], _global_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
+    if let Some(command_line) = get_task_command_map().get(task) {
+        run_command_line(command_line, verbose)
+    } else {
+        Err(report!(KeeperError::ManagerTaskNotFound(task.to_owned(), "mix".to_string())))
+    }
+}
