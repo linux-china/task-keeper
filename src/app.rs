f898c197208ee12b5fdab996e65579d0c4fc1350
linux_china <libing.chen@gmail.com>
2022-08-11 15:18:31 -0700

    build: update to version 0.5.1

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 //! clap App for command cli
 use clap::{Command, Arg};
 
-pub const VERSION: &str = "0.5.0";
+pub const VERSION: &str = "0.5.1";
 
 pub fn build_app() -> Command<'static> {
     Command::new("tk")
