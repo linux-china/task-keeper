af636e8a3b34bdaa3992545553d4b92300e37a8f
linux_china <libing.chen@gmail.com>
2022-10-06 13:18:06 +0800

    fix: bug fix for Gradle multi module

--- src/managers/gradle.rs
+++ src/managers/gradle.rs
@@ -26,7 +26,6 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     task_command_map.insert("install".to_string(), format!("{} classes dependencies", gradle_command));
     task_command_map.insert("compile".to_string(), format!("{} classes testClasses", gradle_command));
     task_command_map.insert("build".to_string(), format!("{} assemble", gradle_command));
-    task_command_map.insert("build".to_string(), get_start_command_line());
     task_command_map.insert("test".to_string(), format!("{} test", gradle_command));
     task_command_map.insert("deps".to_string(), format!("{} dependencies", gradle_command));
     task_command_map.insert("doc".to_string(), format!("{} javadoc", gradle_command));
@@ -67,17 +66,21 @@ pub fn get_gradle_build_file() -> &'static str {
 
 fn get_start_command_line() -> String {
     let build_gradle_file = get_gradle_build_file();
-    let gradle_build_code = std::env::current_dir()
-        .map(|dir| dir.join(build_gradle_file))
-        .map(|path| std::fs::read_to_string(path).unwrap())
-        .unwrap_or("".to_owned());
-    return if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("org.springframework.boot")"#))
-        || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'org.springframework.boot'"#)) {
-        format!("{} bootRun", get_gradle_command())
-    } else if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("io.quarkus")"#))
-        || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'io.quarkus'"#)) {
-        format!("{} --console=plain quarkusDev", get_gradle_command())
+    if std::env::current_dir().map(|dir| dir.join(build_gradle_file).exists()).unwrap_or(false) {
+        let gradle_build_code = std::env::current_dir()
+            .map(|dir| dir.join(build_gradle_file))
+            .map(|path| std::fs::read_to_string(path).unwrap())
+            .unwrap_or("".to_owned());
+        return if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("org.springframework.boot")"#))
+            || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'org.springframework.boot'"#)) {
+            format!("{} bootRun", get_gradle_command())
+        } else if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("io.quarkus")"#))
+            || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'io.quarkus'"#)) {
+            format!("{} --console=plain quarkusDev", get_gradle_command())
+        } else {
+            format!("{} run", get_gradle_command())
+        };
     } else {
         format!("{} run", get_gradle_command())
-    };
+    }
 }
