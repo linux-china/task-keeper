eee1757c03b6482728453b1d0b2a19ee7c522feb
linux_china <libing.chen@gmail.com>
2022-10-20 09:23:54 +0800

    feat: add run_command_line_from_stdin()

--- src/command_utils.rs
+++ src/command_utils.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::Write;
 use std::process::{Command, Output, Stdio};
 use colored::Colorize;
 use error_stack::{IntoReport, report, Result, ResultExt};
@@ -23,6 +24,38 @@ pub fn run_command_line(command_line: &str, verbose: bool) -> Result<Output, Kee
     let args: Vec<&str> = command_and_args[1..].iter().map(AsRef::as_ref).collect();
     if is_command_available(&command_name) {
         run_command(&command_name, &args, verbose)
+    } else {
+        println!("{}", format!("{} is not available to run '{}'", command_name, command_line).bold().red());
+        Err(report!(KeeperError::CommandNotFound(command_name.to_string())))
+    }
+}
+
+pub fn run_command_line_from_stdin(command_line: &str, input: &str, verbose: bool) -> Result<Output, KeeperError> {
+    let command_and_args = shlex::split(command_line).unwrap();
+    let command_name = &command_and_args[0];
+    let args: Vec<&str> = if command_and_args.len() > 1 {
+        command_and_args[1..].iter().map(AsRef::as_ref).collect()
+    } else {
+        vec![]
+    };
+    if verbose {
+        println!("[tk] command line:  {:?}", command_line);
+    }
+    if is_command_available(&command_name) {
+        let mut child = Command::new("deno")
+            .args(&args)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .into_report()
+            .change_context(KeeperError::FailedToRunTasks(format!("{:?}", command_name)))?;
+        child.stdin
+            .as_mut()
+            .ok_or("Child process stdin has not been captured!").unwrap()
+            .write_all(input.as_bytes()).unwrap();
+        child.wait_with_output().into_report()
+            .change_context(KeeperError::FailedToRunTasks(format!("{:?}", command_name)))
     } else {
         println!("{}", format!("{} is not available to run '{}'", command_name, command_line).bold().red());
         Err(report!(KeeperError::CommandNotFound(command_name.to_string())))
@@ -97,11 +130,17 @@ pub fn capture_command_output(command_name: &str, args: &[&str]) -> Result<Outpu
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
     use super::*;
 
     #[test]
     fn test_run_pipe_line() {
         run_command_line("ls -al | wc -l", true).unwrap();
+    }
+
+    #[test]
+    fn test_run_command_line_from_stdin() {
+        run_command_line_from_stdin("deno run -", "console.log('hello world')", true).unwrap();
     }
 
     #[test]
