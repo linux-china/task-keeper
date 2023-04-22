848bc4568c1318f7194c56ef603aa457006ddb97
Libing Chen <libing.chen@gmail.com>
2023-04-22 18:15:19 +0800

    build: update to version 0.11.1

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 //! clap App for command cli
 use clap::{Command, Arg, ArgAction};
 
-pub const VERSION: &str = "0.11.0";
+pub const VERSION: &str = "0.11.1";
 
 pub fn build_app() -> Command {
     Command::new("tk")
