0b8447d4dc499d3503298f94e2f196acf7d99ec2
Libing Chen <libing.chen@gmail.com>
2023-04-27 18:39:55 +0800

    chore: add vendor/bin in PATH

--- src/main.rs
+++ src/main.rs
@@ -212,7 +212,7 @@ fn main() {
 fn reset_path_env() {
     let current_dir = env::current_dir().unwrap();
     let mut new_path = env::var("PATH").unwrap_or_else(|_| "".to_string());
-    for dir in ["bin", ".bin", "node_modules/.bin", "venv/bin", ".venv/bin"].iter() {
+    for dir in ["bin", ".bin", "node_modules/.bin", "venv/bin", ".venv/bin", "vendor/bin"].iter() {
         let bin_path = current_dir.join(dir);
         if bin_path.exists() {
             new_path = format!("{}{}{}", bin_path.to_string_lossy().to_string(), PATH_SEPARATOR, new_path);
@@ -393,6 +393,12 @@ fn diagnose() {
         if !managers::requirements::is_command_available() {
             problems_count += 1;
             println!("{} pip(https://pypi.org/project/pip/) command not available for requirements.txt", "Warning:".bold().yellow());
+        }
+    }
+    if managers::rye::is_available() {
+        if !managers::rye::is_command_available() {
+            problems_count += 1;
+            println!("{} rye(https://github.com/mitsuhiko/rye) command not available for requirements.lock", "Warning:".bold().yellow());
         }
     }
     if managers::rebar3::is_available() {
