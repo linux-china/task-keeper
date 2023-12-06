e69607829d66d6bbe7449eb43b36d66e6bbf197d
Libing Chen <libing.chen@gmail.com>
2023-12-06 23:27:07 +0800

    chore: adjust logic for start

--- src/managers/cargo.rs
+++ src/managers/cargo.rs
@@ -21,13 +21,18 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     task_command_map.insert("install".to_string(), "cargo build".to_string());
     task_command_map.insert("compile".to_string(), "cargo build".to_string());
     task_command_map.insert("build".to_string(), "cargo build".to_string());
-    task_command_map.insert("start".to_string(), "cargo run".to_string());
     task_command_map.insert("test".to_string(), "cargo test".to_string());
     task_command_map.insert("deps".to_string(), "cargo tree".to_string());
     task_command_map.insert("doc".to_string(), "cargo doc".to_string());
     task_command_map.insert("clean".to_string(), "cargo clean".to_string());
     task_command_map.insert("outdated".to_string(), "cargo outdated".to_string());
     task_command_map.insert("update".to_string(), "cargo update".to_string());
+    let cargo_bin = std::env::current_dir()
+        .map(|dir| dir.join("src").join("main.rs").exists())
+        .unwrap_or(false);
+    if cargo_bin {
+        task_command_map.insert("start".to_string(), "cargo run".to_string());
+    }
     task_command_map
 }
 
