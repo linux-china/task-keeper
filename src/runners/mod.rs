287feef6d27023885297dad64aa3dd8c79c2a97e
Libing Chen <libing.chen@gmail.com>
2024-01-23 23:34:16 +0800

    chore: add bun_shell

--- src/runners/mod.rs
+++ src/runners/mod.rs
@@ -16,13 +16,14 @@ pub mod vstasks;
 pub mod ant;
 pub mod rye;
 pub mod poetry;
+pub mod bun_shell;
 
 use std::process::{Output};
 use colored::Colorize;
 use error_stack::{report, Result};
 use crate::errors::KeeperError;
 
-pub const RUNNERS: &'static [&'static str] = &["ant", "rake", "invoke", "task", "cargo-make", "just", "make", "proc", "npm", "deno", "composer", "jbang", "shell", "fleet", "vscode", "markdown", "rye", "poetry"];
+pub const RUNNERS: &'static [&'static str] = &["ant", "rake", "invoke", "task", "cargo-make", "just", "make", "proc", "npm", "deno", "composer", "jbang", "shell", "fleet", "vscode", "markdown", "rye", "poetry","bun-shell"];
 
 pub fn run_task(runner: &str, task_name: &str, task_args: &[&str], global_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
     println!("{}", format!("[tk] execute {} from {}", task_name, runner).bold().blue());
@@ -45,6 +46,7 @@ pub fn run_task(runner: &str, task_name: &str, task_args: &[&str], global_args: 
         "jbang" => jbang::run_task(task_name, task_args, global_args, verbose),
         "rye" => rye::run_task(task_name, task_args, global_args, verbose),
         "poetry" => poetry::run_task(task_name, task_args, global_args, verbose),
+        "bun-shell" => bun_shell::run_task(task_name, task_args, global_args, verbose),
         _ => Err(report!(KeeperError::FailedToRunTasks(format!("Unknown runner: {}", runner)))),
     }
 }
@@ -69,6 +71,7 @@ pub fn get_runner_file_name(runner: &str) -> &'static str {
         "jbang" => "jbang-catalog.json",
         "rye" => "requirements.lock",
         "poetry" => "pyproject.toml",
+        "bun-shell" => "Taskfile.ts",
         _ => "unknown",
     }
 }
@@ -93,6 +96,7 @@ pub fn get_runner_web_url(runner: &str) -> &'static str {
         "jbang" => "https://www.jbang.dev/",
         "rye" => "https://github.com/mitsuhiko/rye",
         "poetry" => "https://python-poetry.org",
+        "bun-shell" => "https://bun.sh/docs/runtime/shell",
         _ => "unknown",
     }
 }
