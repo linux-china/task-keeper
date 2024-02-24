eac120dfc7a4f74a8f48879de1d80843b87ea9b2
Libing Chen <libing.chen@gmail.com>
2024-02-24 13:02:17 +0800

    build: add start if spring boot or quarkus detected

--- src/managers/gradle.rs
+++ src/managers/gradle.rs
@@ -26,12 +26,15 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     task_command_map.insert("install".to_string(), format!("{} --refresh-dependencies classes dependencies", gradle_command));
     task_command_map.insert("compile".to_string(), format!("{} classes testClasses", gradle_command));
     task_command_map.insert("build".to_string(), format!("{} assemble", gradle_command));
+    if let Some(start_command) = get_start_command_line() {
+        task_command_map.insert("start".to_string(), start_command);
+    }
     task_command_map.insert("test".to_string(), format!("{} test", gradle_command));
     task_command_map.insert("deps".to_string(), format!("{} dependencies", gradle_command));
     task_command_map.insert("doc".to_string(), format!("{} javadoc", gradle_command));
     task_command_map.insert("clean".to_string(), format!("{} clean", gradle_command));
     task_command_map.insert("outdated".to_string(), format!("{} dependencyUpdates", gradle_command));
-    if let Ok(code) =std::fs::read_to_string("gradle/wrapper/gradle-wrapper.properties") {
+    if let Ok(code) = std::fs::read_to_string("gradle/wrapper/gradle-wrapper.properties") {
         if !code.contains("gradle-8.6") {
             task_command_map.insert("self-update".to_string(), format!("{} wrapper --gradle-version=8.6", gradle_command));
         }
@@ -69,23 +72,20 @@ pub fn get_gradle_build_file() -> &'static str {
     }
 }
 
-fn get_start_command_line() -> String {
+fn get_start_command_line() -> Option<String> {
     let build_gradle_file = get_gradle_build_file();
     if std::env::current_dir().map(|dir| dir.join(build_gradle_file).exists()).unwrap_or(false) {
         let gradle_build_code = std::env::current_dir()
             .map(|dir| dir.join(build_gradle_file))
             .map(|path| std::fs::read_to_string(path).unwrap())
             .unwrap_or("".to_owned());
-        return if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("org.springframework.boot")"#))
+        if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("org.springframework.boot")"#))
             || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'org.springframework.boot'"#)) {
-            format!("{} bootRun", get_gradle_command())
+            return Some(format!("{} bootRun", get_gradle_command()));
         } else if (build_gradle_file == "build.gradle.kt" && gradle_build_code.contains(r#"id("io.quarkus")"#))
             || (build_gradle_file == "build.gradle" && gradle_build_code.contains(r#"id 'io.quarkus'"#)) {
-            format!("{} --console=plain quarkusDev", get_gradle_command())
-        } else {
-            format!("{} run", get_gradle_command())
-        };
-    } else {
-        format!("{} run", get_gradle_command())
+            return Some(format!("{} --console=plain quarkusDev", get_gradle_command()));
+        }
     }
+    None
 }
