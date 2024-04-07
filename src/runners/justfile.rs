d99bec0e26ae065b5007bfd3eb2683c62c45b947
Libing Chen <libing.chen@gmail.com>
2024-04-07 23:11:01 +0800

    feat: add just module support

--- src/runners/justfile.rs
+++ src/runners/justfile.rs
@@ -13,6 +13,7 @@ use which::which;
 #[serde(rename_all = "camelCase")]
 pub struct JustfileJson {
     pub recipes: HashMap<String, JustRecipe>,
+    pub modules: Option<HashMap<String, JustfileJson>>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -39,16 +40,31 @@ pub fn list_tasks() -> Result<Vec<Task>, KeeperError> {
         })?;
     serde_json::from_str::<JustfileJson>(&json_text)
         .map(|justfile_json| {
-            justfile_json.recipes
+            let mut tasks: Vec<Task> = vec![];
+            // sub tasks
+            let sub_tasks: Vec<Task> = justfile_json.recipes
                 .iter()
                 .map(|(name, recipe)| task!(name, "just", &recipe.doc.clone().unwrap_or("".to_owned())))
-                .collect()
+                .collect();
+            tasks.extend(sub_tasks);
+            if let Some(modules) = justfile_json.modules {
+                for (module_name, justfile_json) in &modules {
+                    let sub_tasks: Vec<Task> = justfile_json.recipes
+                        .iter()
+                        .map(|(name, recipe)| task!(format!("{}::{}", module_name,name ), "just", &recipe.doc.clone().unwrap_or("".to_owned())))
+                        .collect();
+                    tasks.extend(sub_tasks);
+                }
+            }
+            println!("{:?}", tasks);
+            tasks
         })
         .change_context(KeeperError::InvalidJustfile)
 }
 
 pub fn run_task(task: &str, task_args: &[&str], global_args: &[&str], verbose: bool) -> Result<Output, KeeperError> {
     let mut args = vec![];
+    args.push("--unstable");
     args.extend(global_args);
     args.push(task);
     args.extend(task_args);
@@ -68,7 +84,7 @@ mod tests {
 
     #[test]
     fn test_run() {
-        if let Ok(output) = run_task("hello", &[], &[],true) {
+        if let Ok(output) = run_task("hello", &[], &[], true) {
             let status_code = output.status.code().unwrap_or(0);
             println!("exit code: {}", status_code);
         }
