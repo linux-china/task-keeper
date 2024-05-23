26724cdd837264185a2e07e47ef130f2ee2df500
Libing Chen <libing.chen@gmail.com>
2024-05-24 01:33:53 +0800

    chore: add builddir auto creation

--- src/managers/meson.rs
+++ src/managers/meson.rs
@@ -8,7 +8,7 @@ use crate::errors::KeeperError;
 
 pub fn is_available() -> bool {
     std::env::current_dir()
-        .map(|dir| dir.join("meson").exists())
+        .map(|dir| dir.join("meson.build").exists())
         .unwrap_or(false)
 }
 
@@ -20,9 +20,9 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     let mut task_command_map = HashMap::new();
     let build_dir = get_build_dir();
     task_command_map.insert("init".to_string(), "meson setup builddir".to_string());
-    task_command_map.insert("compile".to_string(), format!("meson compile -C {}", build_dir));
-    task_command_map.insert("build".to_string(), format!("meson dist -C {}", build_dir));
-    task_command_map.insert("test".to_string(), format!("build_dir test -C {}", build_dir));
+    task_command_map.insert("compile".to_string(), format!("meson compile -C {}", &build_dir));
+    task_command_map.insert("build".to_string(), format!("meson dist -C {}", &build_dir));
+    task_command_map.insert("test".to_string(), format!("build_dir test -C {}", &build_dir));
     task_command_map
 }
 
@@ -30,8 +30,8 @@ pub fn run_task(task: &str, _task_args: &[&str], _global_args: &[&str], verbose:
     if let Some(command_line) = get_task_command_map().get(task) {
         if task != "init" {
             let build_dir = get_build_dir();
-            if !PathBuf::from(&build_dir).join(build_dir).exists() {
-                run_command_line(&format!("meson setup {}", build_dir), verbose);
+            if !PathBuf::from(&build_dir).join(&build_dir).exists() {
+                let _ = run_command_line(&format!("meson setup {}", build_dir), verbose);
             }
         }
         run_command_line(command_line, verbose)
