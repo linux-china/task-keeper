af96b548fb061e2cdcd967a353fc3a3e669b4b79
Libing Chen <libing.chen@gmail.com>
2024-06-15 00:13:53 +0800

    chore: update to gradle 8.8

--- src/managers/gradle.rs
+++ src/managers/gradle.rs
@@ -35,8 +35,8 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     task_command_map.insert("clean".to_string(), format!("{} clean", gradle_command));
     task_command_map.insert("outdated".to_string(), format!("{} dependencyUpdates", gradle_command));
     if let Ok(code) = std::fs::read_to_string("gradle/wrapper/gradle-wrapper.properties") {
-        if !code.contains("gradle-8.7") {
-            task_command_map.insert("self-update".to_string(), format!("{} wrapper --gradle-version=8.7", gradle_command));
+        if !code.contains("gradle-8.8") {
+            task_command_map.insert("self-update".to_string(), format!("{} wrapper --gradle-version=8.8", gradle_command));
         }
     }
     task_command_map
