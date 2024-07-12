f50c7a4b565c18f19acbbe840cb0a7d3a1262ad7
Libing Chen <libing.chen@gmail.com>
2024-07-12 22:21:11 +0800

    chore: update to maven 3.9.8

--- src/managers/maven.rs
+++ src/managers/maven.rs
@@ -30,8 +30,8 @@ pub fn get_task_command_map() -> HashMap<String, String> {
     task_command_map.insert("outdated".to_string(), format!("{} versions:display-dependency-updates", mvn_command));
     if std::env::current_dir().map(|dir| dir.join(".mvn/wrapper").exists()).unwrap_or(false) {
         if let Ok(code) = std::fs::read_to_string(".mvn/wrapper/maven-wrapper.properties") {
-            if !code.contains("apache-maven-3.9.6") {
-                task_command_map.insert("self-update".to_string(), format!("{} org.apache.maven.plugins:maven-wrapper-plugin:3.3.0:wrapper -Dmaven=3.9.6", mvn_command));
+            if !code.contains("apache-maven-3.9.8") {
+                task_command_map.insert("self-update".to_string(), format!("{} org.apache.maven.plugins:maven-wrapper-plugin:3.3.0:wrapper -Dmaven=3.9.8", mvn_command));
             }
         }
     }
