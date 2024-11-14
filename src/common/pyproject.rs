a000617a6bdb4c2d3e45fe48e79e631389275317
Libing Chen <libing.chen@gmail.com>
2024-11-14 20:45:45 +0800

    feat: add poe task runner support

--- src/common/pyproject.rs
+++ src/common/pyproject.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PyProjectToml {
     tool: Option<Tool>,
@@ -13,6 +12,7 @@ pub struct Tool {
     rye: Option<ToolRye>,
     uv: Option<ToolUv>,
     poetry: Option<ToolPoetry>,
+    poe: Option<PeoTasks>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -27,17 +27,29 @@ pub struct ToolUv {
     scripts: Option<HashMap<String, toml::Value>>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct PeoTasks {
+    tasks: Option<HashMap<String, toml::Value>>,
+}
+
 impl ToolRye {
     pub fn get_scripts(&self) -> Option<HashMap<String, String>> {
         self.scripts.as_ref().map(|scripts| {
-            scripts.iter().map(|(key, value)| {
-                let description: String = match value {
-                    toml::Value::String(value) => value.to_string(),
-                    toml::Value::Table(table) => table.get("cmd").unwrap_or(&toml::Value::String("".to_owned())).to_string(),
-                    _ => "".to_owned()
-                };
-                return (key.clone(), description);
-            }).collect()
+            scripts
+                .iter()
+                .map(|(key, value)| {
+                    let description: String = match value {
+                        toml::Value::String(value) => value.to_string(),
+                        toml::Value::Table(table) => table
+                            .get("cmd")
+                            .unwrap_or(&toml::Value::String("".to_owned()))
+                            .to_string(),
+                        _ => "".to_owned(),
+                    };
+                    return (key.clone(), description);
+                })
+                .collect()
         })
     }
 }
@@ -45,18 +57,48 @@ impl ToolRye {
 impl ToolUv {
     pub fn get_scripts(&self) -> Option<HashMap<String, String>> {
         self.scripts.as_ref().map(|scripts| {
-            scripts.iter().map(|(key, value)| {
-                let description: String = match value {
-                    toml::Value::String(value) => value.to_string(),
-                    toml::Value::Table(table) => table.get("cmd").unwrap_or(&toml::Value::String("".to_owned())).to_string(),
-                    _ => "".to_owned()
-                };
-                return (key.clone(), description);
-            }).collect()
+            scripts
+                .iter()
+                .map(|(key, value)| {
+                    let description: String = match value {
+                        toml::Value::String(value) => value.to_string(),
+                        toml::Value::Table(table) => table
+                            .get("cmd")
+                            .unwrap_or(&toml::Value::String("".to_owned()))
+                            .to_string(),
+                        _ => "".to_owned(),
+                    };
+                    return (key.clone(), description);
+                })
+                .collect()
         })
     }
 }
 
+impl PeoTasks {
+    pub fn get_tasks(&self) -> Option<HashMap<String, String>> {
+        self.tasks.as_ref().map(|tasks| {
+            tasks
+                .iter()
+                .map(|(key, value)| {
+                    let description: String = match value {
+                        toml::Value::String(value) => value.to_string(),
+                        toml::Value::Table(table) => table
+                            .get("help")
+                            .unwrap_or(
+                                table
+                                    .get("cmd")
+                                    .unwrap_or(&toml::Value::String("".to_owned())),
+                            )
+                            .to_string(),
+                        _ => "".to_owned(),
+                    };
+                    return (key.clone(), description);
+                })
+                .collect()
+        })
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "kebab-case")]
@@ -67,54 +109,85 @@ pub struct ToolPoetry {
 impl ToolPoetry {
     pub fn get_scripts(&self) -> Option<HashMap<String, String>> {
         self.scripts.as_ref().map(|scripts| {
-            scripts.iter().map(|(key, value)| {
-                let description: String = match value {
-                    toml::Value::String(value) => value.to_string(),
-                    toml::Value::Table(table) => table.get("callable").unwrap_or(&toml::Value::String("".to_owned())).to_string(),
-                    _ => "".to_owned()
-                };
-                return (key.clone(), description);
-            }).collect()
+            scripts
+                .iter()
+                .map(|(key, value)| {
+                    let description: String = match value {
+                        toml::Value::String(value) => value.to_string(),
+                        toml::Value::Table(table) => table
+                            .get("callable")
+                            .unwrap_or(&toml::Value::String("".to_owned()))
+                            .to_string(),
+                        _ => "".to_owned(),
+                    };
+                    return (key.clone(), description);
+                })
+                .collect()
         })
     }
 }
 
 impl PyProjectToml {
     pub fn get_default_project() -> Result<Self, std::io::Error> {
-        if let Ok(pyproject_file) = std::env::current_dir()
-            .map(|dir| {
-                dir.join("pyproject.toml")
-            }) {
+        if let Ok(pyproject_file) = std::env::current_dir().map(|dir| dir.join("pyproject.toml")) {
             let text = std::fs::read_to_string(pyproject_file).unwrap_or("".to_owned());
-            toml::from_str(&text).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))
+            toml::from_str(&text)
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))
         } else {
-            Err(std::io::Error::new(std::io::ErrorKind::NotFound, "pyproject.toml not found").into())
+            Err(
+                std::io::Error::new(std::io::ErrorKind::NotFound, "pyproject.toml not found")
+                    .into(),
+            )
         }
     }
 
     pub fn rye_available(&self) -> bool {
-        self.tool.as_ref().map(|tool| tool.rye.is_some()).unwrap_or(false)
+        self.tool
+            .as_ref()
+            .map(|tool| tool.rye.is_some())
+            .unwrap_or(false)
     }
 
     pub fn uv_available(&self) -> bool {
-        self.tool.as_ref().map(|tool| tool.uv.is_some()).unwrap_or(false)
+        self.tool
+            .as_ref()
+            .map(|tool| tool.uv.is_some())
+            .unwrap_or(false)
     }
 
     pub fn poetry_available(&self) -> bool {
-        self.tool.as_ref().map(|tool| tool.poetry.is_some()).unwrap_or(false)
+        self.tool
+            .as_ref()
+            .map(|tool| tool.poetry.is_some())
+            .unwrap_or(false)
     }
 
     pub fn get_rye_scripts(&self) -> Option<HashMap<String, String>> {
-        self.tool.as_ref().and_then(|tool| tool.rye.as_ref()).and_then(|rye| rye.get_scripts().clone())
+        self.tool
+            .as_ref()
+            .and_then(|tool| tool.rye.as_ref())
+            .and_then(|rye| rye.get_scripts().clone())
     }
 
     pub fn get_uv_scripts(&self) -> Option<HashMap<String, String>> {
-        self.tool.as_ref().and_then(|tool| tool.uv.as_ref()).and_then(|uv| uv.get_scripts().clone())
+        self.tool
+            .as_ref()
+            .and_then(|tool| tool.uv.as_ref())
+            .and_then(|uv| uv.get_scripts().clone())
     }
 
-
     pub fn get_poetry_scripts(&self) -> Option<HashMap<String, String>> {
-        self.tool.as_ref().and_then(|tool| tool.poetry.as_ref()).and_then(|poetry| poetry.get_scripts().clone())
+        self.tool
+            .as_ref()
+            .and_then(|tool| tool.poetry.as_ref())
+            .and_then(|poetry| poetry.get_scripts().clone())
+    }
+
+    pub fn get_poe_tasks(&self) -> Option<HashMap<String, String>> {
+        self.tool
+            .as_ref()
+            .and_then(|tool| tool.poe.as_ref())
+            .and_then(|peo_tasks| peo_tasks.get_tasks().clone())
     }
 }
 
@@ -125,6 +198,7 @@ mod tests {
     #[test]
     fn test_get_default_project() {
         let pyproject = PyProjectToml::get_default_project().unwrap();
-        println!("{:#?}", pyproject);
+        // println!("{:#?}", pyproject);
+        println!("{:#?}", pyproject.get_poe_tasks());
     }
 }
