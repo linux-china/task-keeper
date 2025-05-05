f1728d1d8c96c88ebdb3d7645cf7d92e2b693d2e
Libing Chen <libing.chen@gmail.com>
2025-05-05 17:07:27 +0800

    test: polishing

--- tests/demo_test.rs
+++ tests/demo_test.rs
@@ -1,4 +1,15 @@
+use std::process::Command;
+
 #[test]
 fn test_demo() {
     println!("demo test");
+}
+
+#[test]
+fn test_command() {
+    let output = Command::new("java")
+        .arg("-version")
+        .output()
+        .expect("Failed to execute command");
+    String::from_utf8(output.stderr).unwrap();
 }
