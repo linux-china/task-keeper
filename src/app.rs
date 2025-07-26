8aca2f5640ab80dfaa702d917113adcf3c9ead29
Libing Chen <libing.chen@gmail.com>
2025-07-25 18:23:20 -0700

    release: update to version 0.30.0

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 //! clap App for command cli
 use clap::{Command, Arg, ArgAction};
 
-pub const VERSION: &str = "0.29.3";
+pub const VERSION: &str = "0.30.0";
 
 pub fn build_app() -> Command {
     Command::new("tk")
