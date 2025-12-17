c894917458a4544214e75c5349f1794b3d5759de
Liging Chen <libing.chen@gmail.com>
2025-12-17 18:36:55 +0800

    chore: update to maven 3.9.12

--- src/managers/maven.rs
+++ src/managers/maven.rs
@@ -59,8 +59,8 @@ pub fn get_task_command_map() -> HashMap<String, String> {
         .unwrap_or(false)
     {
         if let Ok(code) = std::fs::read_to_string(".mvn/wrapper/maven-wrapper.properties") {
-            if !code.contains("apache-maven-3.9.11") {
-                task_command_map.insert("self-update".to_string(), format!("{} org.apache.maven.plugins:maven-wrapper-plugin:3.3.4:wrapper -Dmaven=3.9.11", mvn_command));
+            if !code.contains("apache-maven-3.9.12") {
+                task_command_map.insert("self-update".to_string(), format!("{} org.apache.maven.plugins:maven-wrapper-plugin:3.3.4:wrapper -Dmaven=3.9.12", mvn_command));
             }
         }
     }
