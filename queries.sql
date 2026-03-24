22190c1729f6016934729f72afe0003b4314c187
Liging Chen <libing.chen@gmail.com>
2026-03-24 10:32:31 +0800

    chore: use duckdb memory

--- queries.sql
+++ queries.sql
@@ -1,7 +1,10 @@
--- DSN_URL=sqlite://./demo.sqlite3
+-- DSN_URL=duckdb::memory:
 
--- QUERY get_all_users
-SELECT * FROM users;
+-- QUERY version
+SELECT version();
 
 -- QUERY get_user :one
 SELECT * FROM users WHERE id = 1;
+
+-- QUERY extensions-statics
+UNPIVOT (SELECT 'community' AS repository, * FROM 'https://community-extensions.duckdb.org/downloads-last-week.json') ON COLUMNS(* EXCLUDE (_last_update, repository)) INTO NAME extension VALUE downloads_last_week ORDER BY downloads_last_week DESC;
